d52872eb91ab0ec1816f2719e0b1905b8e19376a
savita <savitads24@gmail.com>
2018-08-14 22:42:41 +0530

    Log4J is added

--- src/Scripts/TestValidLogin.java
+++ src/Scripts/TestValidLogin.java
@@ -1,6 +1,8 @@
 package Scripts;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -16,11 +18,16 @@ public class TestValidLogin extends BaseTest{
 	@Test(invocationCount=2)
 	public void validLogin() throws InterruptedException
 	{
+		Logger log = LogManager.getLogger(TestValidLogin.class.getName());
+		log.debug("creating the object of Login Page");
 		LoginPage login = new LoginPage(driver);
+		log.info("Login page object is created successfully");
 		String userName = Lib.getCellValue("ValidLoginCredentials", 0, 0);
 		login.setUserName(userName);
 		login.setPassword(Lib.getCellValue("ValidLoginCredentials", 0, 1));
+		log.debug("Username and password entered");
 		login.clickLogin();
+		log.info("Login button is clicked");
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions.titleIs("actiTIME - Enter Time-Track"));
 		//wait.until(ExpectedConditions.titleIs("Title of the homepage"));
